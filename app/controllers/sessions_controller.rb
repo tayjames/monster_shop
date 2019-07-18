369574c829fc7fe86a4b3c8a8d9effc8960f4552
Tay <tay.james.d@gmail.com>
2019-07-17 19:25:15 -0600

    Updates new method to include nested conditional so a user who is already logged in will be taken to their profile/dashboard if they visit login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
   def new
+    # binding.pry
+    if current_user
+      if current_registered_user?
+        redirect_to (user_profile_path(current_user))
+        flash[:success] = "You are already logged in"
+      elsif current_merchant?
+        redirect_to (merchant_dashboard_path)
+        flash[:success] = "You are already logged in"
+      else current_admin?
+        redirect_to (admin_dashboard_path)
+        flash[:success] = "You are already logged in"
+      end
+    end
+      # redirect_to (user_profile_path(current_user)) if current_registered_user?
+      # redirect_to (merchant_dashboard_path) if current_merchant?
   end
 
   def create
