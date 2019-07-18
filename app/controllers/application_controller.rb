ac268364462cb3f23e94b2682acb41106470ec4a
Tay <tay.james.d@gmail.com>
2019-07-18 14:57:28 -0600

    Adds current_merchant_admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_registered_user?, :current_admin?, :current_merchant?
+  helper_method :cart, :current_user, :current_registered_user?, :current_admin?, :current_merchant_user?, :current_merchant_admin?
 
 
   def cart
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    # binding.pry
   end
 
   def generate_flash(resource)
@@ -18,20 +17,23 @@ class ApplicationController < ActionController::Base
       flash[validation] = "#{validation}: #{message}"
     end
   end
-  #
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
 
   def current_registered_user?
     current_user && current_user.role == "registered_user" #need to include and statment or else user will come back nil in login spec
+    # current_user && current_registered_user?
   end
 
-  def current_merchant?
+  def current_merchant_user?
     current_user && current_user.merchant_id
+
+  end
+
+  def current_merchant_admin?
+    current_user && current_user.role == "merchant_admin"
   end
 
   def current_admin?
     current_user && current_user.role == "admin"
+    # current_user && current_admin?
   end
 end
