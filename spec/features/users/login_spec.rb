47088363f15be7d26c4087c1e5ea736de10975f5
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-07-18 09:14:19 -0600

    Add user who works for merchant navigation, as well as login

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -54,5 +54,28 @@ RSpec.describe "User Login" do
       expect(page).to have_content("You are now Logged out.")
       expect(page).to have_content("Cart: 0")
     end
+
+    it "I see all normal links, as well as merchant links, as I work for a merchant" do
+      megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      ea = megan.users.create!(email: "yee@email.com", password: "password", name: "Suki", address: "ABC 123 St.", city: "Elwood", state: "DC", zip: 90897, role: 1)
+      user = User.create!(email: "123@gmail.com", password: "password", name: "Tom", role: 1, address: "123 Main St.", city: "Denver", state: "Colorado", zip: 80220)
+
+      visit login_path
+
+      fill_in "email", with: "yee@email.com"
+      fill_in "password", with: "password"
+
+      click_button("Login")
+
+      expect(current_path).to eq(merchant_dashboard_path)
+      expect(page).to have_link("Logout")
+      expect(page).to have_link("Items")
+      expect(page).to have_link("Merchants")
+      expect(page).to have_link("Profile")
+      expect(page).to have_link("Cart: 0")
+
+      expect(page).to_not have_link("Login")
+      expect(page).to_not have_link("Register")
+    end
   end
 end
